0dab064b864094096eeb99eed14730c7e1398efe
sudosf <sudosprogrammer@gmail.com>
2022-07-21 16:49:16 +0200

    added task4

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -11,10 +11,28 @@ public class SimpleFunctions {
         for (Integer i : b)
             if(i < a.size() && i >=0 )
                 sum += a.get(i);
-            
+
         return sum;
     }
-    
+
+    public static boolean Task3 (String message) {
+
+        int numLeftBracket = 0;
+        int numRightBracket = 0;
+
+        for (int i = 0; i < message.length(); i++) {
+
+            if ( message.charAt(i) == '(' ) {
+                numLeftBracket++;
+                if ( message.charAt(i) == ')' ) numRightBracket++;
+            }
+        }
+
+        if (numLeftBracket == 0 || numRightBracket == 0) return false;
+
+        return numLeftBracket == numRightBracket;
+    }
+
     public List<Integer> Task4(List<Integer> a, List<Integer> b) {
         int aLen = a.size();
         int bLen = b.size();
@@ -24,12 +42,18 @@ public class SimpleFunctions {
             return null;
         }
         else {
-            
+
             for (int i = 0; i < aLen; i++) {
                 res = a.get(i) * b.get(bLen - i);
                 arr.add(res);
-            }  
+            }
         }
         return arr;
+    }
+
+    public static void main(String[] args) {
+        // test methods
+
+        System.out.println("SHould be false: " + Task3("msg (()"));
     }
 }
