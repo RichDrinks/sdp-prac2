dfd174174edf810a4e4c868d9a01ea26ac308bc0
Richard <richard.drinkwater.3@gmail.com>
2022-07-21 13:02:59 +0200

    Added Task1 to simpleFunctions by Richard

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,14 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public static int Task1(List<Integer> a, List<Integer> b) //Added by Richard g20d3020
+    {
+        int sum = 0;
+        for (Integer i : b)
+            if(i < a.size() && i >=0 )
+                sum += a.get(i);
+            
+        return sum;
+    }   
 }
