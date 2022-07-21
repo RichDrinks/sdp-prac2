b71bedb77c20be2d28f0e26cfac1ba28466cb481
Khanyisile Cebani <G20C7968@ict.ru.ac.za>
2022-07-21 15:42:24 +0200

    Task6 commit

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -14,4 +14,32 @@ public class SimpleFunctions {
             
         return sum;
     }   
+    public List<Integer> Task6(List<Integer> listt) {
+        List<Integer> arr=new ArrayList<Integer>();
+        for (int i = 0; i < listt.size(); i++) {
+            //System.out.println(listt.get(i));
+            int val;
+            val = listt.get(i);
+            if (val ==100 || val==200 || val==300 || val==400 || val==500 || val==600 || val==700 || val==800 || val==900 || val==1000 ) {
+                arr.add(val);
+                
+            }else {
+                int ans;
+                ans=Math.round(val / 100)*100;
+                arr.add(ans);
+                
+            }
+            i++;
+        }
+        return arr;
+        //System.out.println(Arrays.toString(arr.toArray()));
+        
+
+
+    }
+
+
+    }
+
+
 }
